7f5db81734fbbdd569c8036d2bf0eba087f0101f
wbrm77 <wbrm77@gmail.com>
2020-06-30 19:25:17 -0400

    Menu formatting

--- HangOut/app/src/main/java/com/example/hangout/MenuActivity.kt
+++ HangOut/app/src/main/java/com/example/hangout/MenuActivity.kt
@@ -4,25 +4,39 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
 
     private lateinit var map1: Button
+    private lateinit var map2: ImageView
     private lateinit var friends: Button
+    private lateinit var friends_icon: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
         map1 = findViewById(R.id.map)
+        map2 = findViewById(R.id.map_icon)
         friends = findViewById(R.id.friends)
+        friends_icon = findViewById(R.id.friends_icon)
 
         map1.setOnClickListener {
             val intent = Intent(this, MapsActivity::class.java)
             startActivity(intent)
         }
+        map2.setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
 
         friends.setOnClickListener {
+            val intent = Intent(this, FriendsActivity::class.java)
+            startActivity(intent)
+        }
+        friends_icon.setOnClickListener {
             val intent = Intent(this, FriendsActivity::class.java)
             startActivity(intent)
         }
